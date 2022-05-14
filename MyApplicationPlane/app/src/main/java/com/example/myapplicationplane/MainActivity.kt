700070849eabd231c4c1a65082306c7695dc865e
ivan-melehin <104354349+ivan-melehin@users.noreply.github.com>
2022-05-14 18:54:53 +0300

    MyApplicationPlane
    There is an Add button on the main screen
    Pressing it displays a screen with a choice of pictures (you can change it by pressing the Next button)
    You can also add a name and description
    And by pressing Done a picture with the name will appear on the main screen
    Made in the style of repeat after the teacher. Source YouTube

---
+++ MyApplicationPlane/app/src/main/java/com/example/myapplicationplane/MainActivity.kt
@@ -0,0 +1,41 @@
+ package com.example.myapplicationplane
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplicationplane.databinding.ActivityMainBinding
+
+ class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter= PlantAdapter()
+     private var editLauncher: ActivityResultLauncher<Intent>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+        editLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == RESULT_OK){
+                adapter.addplant(it.data?.getSerializableExtra("plant") as Plant )
+            }
+        }
+    }
+
+     private fun init (){
+      binding.apply {
+          rcView.layoutManager = GridLayoutManager(this@MainActivity,3)
+          rcView.adapter= adapter
+          buttonAdd.setOnClickListener{
+              editLauncher?.launch(Intent (this@MainActivity, EditActivity::class.java))
+          }
+      }
+
+     }
+
+ }
---
+++ MyApplicationPlane/app/src/main/java/com/example/myapplicationplane/Plant.kt
@@ -0,0 +1,5 @@
+package com.example.myapplicationplane
+
+import java.io.Serializable
+
+data class Plant(val imageId: Int,var title: String, val desc: String) :Serializable
